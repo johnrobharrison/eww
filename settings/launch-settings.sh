0b4abb8a7e966c9439add26a8febc6aedccf6782
shin <shin@starbuck>
2022-12-16 22:54:26 +0000

    fixes

--- settings/launch-settings.sh
+++ settings/launch-settings.sh
@@ -2,19 +2,12 @@
 
 ## Files and cmd
 FILE="$HOME/.config/eww/settings/touch"
-CFG="$HOME/.config/eww/settings/"
-EWW=`which eww`
-
-## Open widgets 
-run_eww() {
-	${EWW} --config "$CFG" open settings
-}
 
 ## Launch or close widgets accordingly
 if [[ ! -f "$FILE" ]]; then
 	touch "$FILE"
-	run_eww
+	eww --config ~/.config/eww/settings open settings 
 else
-	${EWW} --config "$CFG" close settings
+	eww --config ~/.config/eww/settings close settings 
 	rm "$FILE"
 fi
